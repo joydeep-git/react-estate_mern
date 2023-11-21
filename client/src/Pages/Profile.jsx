c826fa71d6493a46b64cdcaafc1a6b796aa0ca93
JOYDEEP DAS <joydeep.accounts@tuta.io>
2023-11-21 21:03:21 +0530

    image upload function created

--- client/src/Pages/Profile.jsx
+++ client/src/Pages/Profile.jsx
@@ -1,25 +1,91 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { FaEyeSlash, FaPen } from "react-icons/fa";
 import { IoEyeOutline } from "react-icons/io5";
 
+import { CircularProgressBar } from "react-percentage-bar";
+
 import { useSelector } from "react-redux";
+
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import { app } from "../Firebase.js";
 
 const Profile = () => {
 
   const { currentUser } = useSelector(state => state.user);
 
-  const [userData, setUserData] = useState(currentUser);
+  const [userData, setUserData] = useState({
+    ...currentUser,
+    password: ""
+  });
+
   const [showPassword, setShowPassword] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const [file, setFile] = useState(undefined);
+  const [uploadPerc, setUploadPerc] = useState(0);
+  const [error, setError] = useState(false);
+  const [errorText, setErrorText] = useState('');
 
   const handleChange = (e) => {
-
     setUserData({
       ...userData,
       [e.target.name]: e.target.value
     });
-
   };
+
+  useEffect(() => {
+    if (file) {
+      handleFile(file);
+    }
+  }, [file]);
+
+  const handleFile = (img) => {
+
+    const storage = getStorage(app);
+
+    const imgName = new Date().getTime() + img.name;
+
+    const storageRef = ref(storage, imgName);
+
+    const uploadImg = uploadBytesResumable(storageRef, img);
+
+    uploadImg.on("state_changed", (snapshot) => {
+
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+      setUploadPerc(Math.round(progress));
+    },
+      (error) => {
+        setError(true);
+        setErrorText("IMAGE UPLOAD FAILED");
+        console.log(error);
+      },
+      () => {
+        getDownloadURL(uploadImg.snapshot.ref).then(
+          (downloadUrl) => {
+            setUserData({
+              ...userData,
+              avatar: downloadUrl
+            })
+            setError(true);
+            setErrorText("PLEASE WAIT A FEW SECONDS TO SEE CHANGES");
+          }
+        )
+      }
+    )
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setError(false);
+    }, 2000);
+  }, [error]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setErrorText('');
+    }, 3000);
+  }, [errorText]);
 
   return (
     <div className="flex flex-col items-center justify-between my-6 p-3 ">
@@ -29,10 +95,52 @@ const Profile = () => {
       <form
         className='flex flex-col gap-4 items-center bg-slate-100 p-6 rounded-md max-w-md w-full'>
 
-        <img
-          className="rounded-full w-full"
-          src={currentUser.avatar}
-          alt="Image" />
+        <div
+          className="relative flex items-center justify-center m-auto w-44 h-44 rounded-full"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+          {
+            uploadPerc === 0 || uploadPerc === 100
+              ? <img
+                className={`rounded-full w-full h-full transition-all ${isHovered ? "filter grayscale(80%)" : ""
+                  }`}
+                src={userData.avatar || currentUser.avatar}
+                alt="Image"
+              />
+              : <CircularProgressBar
+                animation={true}
+                roundLineCap={true}
+                percentage={uploadPerc}
+                text={'Uploading'}
+                percentageAnimation={true} />
+          }
+
+          {isHovered && (
+            <label
+              title="Change Image"
+              htmlFor="file"
+              className=" absolute flex item-center justify-center bg-gray-100 opacity-50 text-xl rounded-md [clip-path: circle] cursor-pointer w-full h-full rounded-full"
+            >
+              <FaPen className="flex self-center" />
+            </label>
+          )}
+
+          <input
+            type="file"
+            name="file"
+            id="file"
+            className="absolute hidden"
+            onChange={(e) => setFile(e.target.files[0])}
+            accept="image/*"
+          />
+        </div>
+
+        {
+          error
+            ? <p className="text-red-600">{errorText}</p>
+            : <p>UPLOAD IMAGES UPTO 2 MB</p>
+        }
 
         <input
           type="text"
@@ -62,6 +170,7 @@ const Profile = () => {
             id='password'
             placeholder='Password'
             className='border-none p-2 rounded-md w-full focus:outline-none  placeholder:text-xl'
+            value={userData.password}
             onChange={handleChange}
           />
 
